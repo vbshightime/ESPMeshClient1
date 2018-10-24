7183f8d4c505cda8b3e787a0a457d9b1a02c2f84
vaibhav sharma <vbsmandi@gmail.com>
2018-10-24 22:31:33 +0530

    Update espMEshClient3_1.ino

--- espMEshClient3_1.ino
+++ espMEshClient3_1.ino
@@ -8,8 +8,8 @@
 # include "painlessMesh.h"
 # include "DHT.h"
 
-#define MESH_SSID "ESPMesh"
-#define MESH_PASS "24041990"
+#define MESH_SSID "Your Mesh ssid"
+#define MESH_PASS "Your Mesh Pass"
 #define MESH_PORT 5555
 #define DHTPIN 0
 #define DHTTYPE DHT11
@@ -76,16 +76,6 @@ myLoggingTask.enable();
 
 
 void loop() {
-/*if(millis()-DHTTimer <= DHTInterval){
-    DHTTimer =millis();
-    temperature= dht.readTemperature();
-    humidity= dht.readHumidity();
-    if (isnan(temperature)&&isnan(humidity)) {
-      temperature=31.00;
-      humidity=85.00;
-      return;
-    }
-  }*/
   
 userScheduler.execute();
 mesh.update();
